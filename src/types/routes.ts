fe791b327ab525937955c5c58b8265540334cc3d
Gbolahan Akande <geakande@gmail.com>
2026-01-21 06:44:24 +0100

    Add type definitions for React Router

---
+++ src/types/routes.ts
@@ -0,0 +1,16 @@
+/**
+ * Route Type Definitions
+ */
+
+export interface RouteConfig {
+  path: string;
+  label: string;
+  component: React.LazyExoticComponent<React.ComponentType<any>>;
+}
+
+export type AppRoutes = {
+  home: string;
+  about: string;
+  howItWorks: string;
+  app: string;
+};
