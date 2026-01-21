a180f6ffc0bf84ae7571c9b02d13d4c6ba42419d
Gbolahan Akande <geakande@gmail.com>
2026-01-21 06:57:36 +0100

    Add TypeScript interfaces for components

---
+++ src/types/components.ts
@@ -0,0 +1,17 @@
+/**
+ * Component Prop Type Definitions
+ */
+
+export interface NavigationProps {}
+
+export interface WalletConnectProps {
+  onConnect?: (address: string) => void;
+}
+
+export interface StrategyDashboardProps {
+  walletAddress?: string;
+}
+
+export interface ErrorBoundaryProps {
+  children: React.ReactNode;
+}
