1c88e92971f718692cee26d951978b794dc10b23
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:48:00 +0100

    tx: add BASE_FEE_RATE constant

--- src/lib/transactions/fee-estimation.ts
+++ src/lib/transactions/fee-estimation.ts
@@ -5,3 +5,5 @@ export const MINIMUM_FEE = 180n;
 export const HIGH_PRIORITY_FEE_MULTIPLIER = 2;
 /** Low priority fee (50% of standard) */
 export const LOW_PRIORITY_FEE_MULTIPLIER = 0.5;
+/** Standard base fee rate (microSTX per byte) */
+export const BASE_FEE_RATE = 1;
