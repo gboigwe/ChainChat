447670f274deabd36a6fc51214425e5793ea0017
Gbolahan Akande <geakande@gmail.com>
2026-03-26 14:34:00 +0100

    api: add RATE_LIMITER_CONST_48

--- src/lib/api/rate-limiter.ts
+++ src/lib/api/rate-limiter.ts
@@ -92,3 +92,4 @@ export const RATE_LIMITER_CONST_44 = 44;
 export const RATE_LIMITER_CONST_45 = 45;
 export const RATE_LIMITER_CONST_46 = 46;
 export const RATE_LIMITER_CONST_47 = 47;
+export const RATE_LIMITER_CONST_48 = 48;
