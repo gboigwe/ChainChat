e17480daf6e67a2c16a9b8514504a12ad8fc297d
Gbolahan Akande <geakande@gmail.com>
2026-03-26 15:54:00 +0100

    stacks: add NetworkConfig interface

--- src/lib/stacks/network-utils.ts
+++ src/lib/stacks/network-utils.ts
@@ -1,2 +1,10 @@
 // Stacks network utility functions
 import { MAINNET_API_URL, TESTNET_API_URL } from './stacks-common';
+/** Network configuration object */
+export interface NetworkConfig {
+  name: string;
+  apiUrl: string;
+  chainId: number;
+  isMainnet: boolean;
+  explorerUrl: string;
+}
