d2cdf6369a149625b17185e89c8702cf401652ab
Gbolahan Akande <geakande@gmail.com>
2026-03-26 12:26:00 +0100

    connect: add CONNECT_CONST_1

--- src/lib/connect/index.ts
+++ src/lib/connect/index.ts
@@ -5,3 +5,5 @@ export * from './stx-transfer';
 export * from './message-signing';
 export * from './providers';
 export * from './session-helpers';
+// connect module constant 1
+export const CONNECT_CONST_1 = 1;
