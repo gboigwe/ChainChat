04decfc76211a394015c891bff06de2f39516800
Gbolahan Akande <geakande@gmail.com>
2026-03-26 18:26:00 +0100

    examples: add EXAMPLES_EXTRA_2

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -3,3 +3,4 @@ export * from './create-channel';
 export const EXAMPLES_VERSION = '1.0.0';
 export const EXAMPLES_PACKAGE = 'chainchat-examples';
 export const EXAMPLES_EXTRA_1 = 1;
+export const EXAMPLES_EXTRA_2 = 2;
