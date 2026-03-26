ec0add806fe62866e09d556f9e858a4891df91e2
Gbolahan Akande <geakande@gmail.com>
2026-03-26 18:00:00 +0100

    utils: add index barrel export

---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+export * from './formatting';
+export * from './validation';
+export * from './conversion';
+export * from './async-helpers';
+export * from './storage-helpers';
+export const UTILS_VERSION = '1.0.0';
+export const UTILS_PACKAGE = 'chainchat-utils';
