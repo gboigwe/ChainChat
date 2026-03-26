60db3ae17505ab552b3d735e2d237a0c78d1c603
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:02:00 +0100

    utils: add FORMAT_CONST_4 formatting constant

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -70,3 +70,4 @@ export function formatFeeRate(fee: bigint, txSize: number): string {
   const rate = Number(fee) / txSize;
   return rate.toFixed(2) + ' μSTX/byte';
 }
+export const FORMAT_CONST_4 = 4;
