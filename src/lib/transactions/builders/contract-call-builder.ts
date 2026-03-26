3127d29bc856a7d3fda8fdaf3effacce8e38969b
Gbolahan Akande <geakande@gmail.com>
2026-03-26 16:22:00 +0100

    tx: add CC_BUILDER_CONST_9

--- src/lib/transactions/builders/contract-call-builder.ts
+++ src/lib/transactions/builders/contract-call-builder.ts
@@ -106,3 +106,4 @@ export const CC_BUILDER_CONST_5 = 5;
 export const CC_BUILDER_CONST_6 = 6;
 export const CC_BUILDER_CONST_7 = 7;
 export const CC_BUILDER_CONST_8 = 8;
+export const CC_BUILDER_CONST_9 = 9;
