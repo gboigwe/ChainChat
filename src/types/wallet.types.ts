23e962a7c621de641866490a29210c0bce202c46
Gbolahan Akande <geakande@gmail.com>
2026-03-26 15:18:00 +0100

    types: add WALLET_TYPE_CONST_15

--- src/types/wallet.types.ts
+++ src/types/wallet.types.ts
@@ -50,3 +50,4 @@ export const WALLET_TYPE_CONST_11 = 11;
 export const WALLET_TYPE_CONST_12 = 12;
 export const WALLET_TYPE_CONST_13 = 13;
 export const WALLET_TYPE_CONST_14 = 14;
+export const WALLET_TYPE_CONST_15 = 15;
