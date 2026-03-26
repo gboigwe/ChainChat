adb052178ce80557a0f2bd6495796c0ebff67b97
Gbolahan Akande <geakande@gmail.com>
2026-03-26 11:10:00 +0100

    tx: add NonceTrackerState interface

--- src/lib/transactions/nonce-management.ts
+++ src/lib/transactions/nonce-management.ts
@@ -1,1 +1,8 @@
 // Nonce management for Stacks transactions
+/** Nonce tracker state */
+export interface NonceTrackerState {
+  address: string;
+  lastFetched: number;
+  nonce: number;
+  pending: number[];
+}
