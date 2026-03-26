8c75ea8c43b4727d0a7d5acd8cd0eb6df12e8dde
Gbolahan Akande <geakande@gmail.com>
2026-03-26 16:44:00 +0100

    clarity: add hexToBigint decoder

--- src/lib/clarity/encoding-utils.ts
+++ src/lib/clarity/encoding-utils.ts
@@ -3,3 +3,7 @@
 export function bigintToHex(value: bigint): string {
   return value.toString(16).padStart(32, '0');
 }
+/** Decode hex string to bigint */
+export function hexToBigint(hex: string): bigint {
+  return BigInt('0x' + hex.replace(/^0x/, ''));
+}
