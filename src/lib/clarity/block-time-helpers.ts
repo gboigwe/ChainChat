d44b4187d20260b3519a8ff5f9e6e73467873c6c
Gbolahan Akande <geakande@gmail.com>
2026-03-26 12:10:00 +0100

    clarity: add BLOCKS_PER_HOUR constant

--- src/lib/clarity/block-time-helpers.ts
+++ src/lib/clarity/block-time-helpers.ts
@@ -2,3 +2,6 @@
 
 /** Average Stacks block time in seconds (~10 min) */
 export const AVERAGE_BLOCK_TIME_SECONDS = 600;
+
+/** Blocks per hour on Stacks mainnet */
+export const BLOCKS_PER_HOUR = 6;
