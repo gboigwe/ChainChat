39173953dc2328ad11a2aec044aa8623ccc93d85
Gbolahan Akande <geakande@gmail.com>
2026-03-26 13:12:00 +0100

    storage: add isValidNFTUri validator

--- src/lib/clarity/storage/validators.ts
+++ src/lib/clarity/storage/validators.ts
@@ -1,2 +1,6 @@
 // Storage operation validators
 import { NFT_URI_MAX_LENGTH, NFT_MAX_SUPPLY, FT_MAX_SUPPLY } from './constants';
+/** Validate NFT URI length */
+export function isValidNFTUri(uri: string): boolean {
+  return uri.length > 0 && uri.length <= NFT_URI_MAX_LENGTH;
+}
