c7c5d0016d82b66fdab6477428b3bc2b07a75eb9
Gbolahan Akande <geakande@gmail.com>
2026-03-26 15:42:00 +0100

    clarity: add ERR_CHANNEL_NOT_FOUND error code

--- src/lib/clarity/error-codes.ts
+++ src/lib/clarity/error-codes.ts
@@ -1,3 +1,5 @@
 // Clarity contract error code registry
 /** Error: sender is not a channel member */
 export const ERR_NOT_MEMBER = 100n;
+/** Error: channel does not exist */
+export const ERR_CHANNEL_NOT_FOUND = 101n;
