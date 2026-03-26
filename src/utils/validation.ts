df56d735ce742356b6460f9f1da463b56297f177
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:40:00 +0100

    utils: add VALID_CONST_7 validation constant

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -5,3 +5,4 @@ export const VALIDATION_MIN_USERNAME_LEN = 2;
 export const VALIDATION_MAX_USERNAME_LEN = 32;
 export const VALIDATION_HEX_EVEN_LENGTH = true;
 export const VALID_CONST_6 = 6;
+export const VALID_CONST_7 = 7;
