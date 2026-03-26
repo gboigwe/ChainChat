2e5932c8149d775966bcdc2963f19c5558bcca52
Gbolahan Akande <geakande@gmail.com>
2026-03-26 14:12:00 +0100

    types: add CV_TYPE_CONST_27

--- src/types/clarity-value.types.ts
+++ src/types/clarity-value.types.ts
@@ -38,3 +38,4 @@ export const CV_TYPE_CONST_23 = 23;
 export const CV_TYPE_CONST_24 = 24;
 export const CV_TYPE_CONST_25 = 25;
 export const CV_TYPE_CONST_26 = 26;
+export const CV_TYPE_CONST_27 = 27;
