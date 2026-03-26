caa601e174a029ccba02db774c4f5a60455be6c9
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:38:00 +0100

    tx: add STXTransferTxOptions interface

--- src/lib/transactions/builders/stx-transfer-builder.ts
+++ src/lib/transactions/builders/stx-transfer-builder.ts
@@ -1,1 +1,12 @@
 // STX transfer transaction builder
+/** STX transfer transaction options */
+export interface STXTransferTxOptions {
+  recipient: string;
+  amount: bigint;
+  memo: string;
+  fee: bigint;
+  nonce: bigint;
+  anchorMode: 'on-chain-only' | 'off-chain-only' | 'any';
+  network: string;
+  senderKey?: string;
+}
