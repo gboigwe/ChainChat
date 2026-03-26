07c9188b7d9e0529ba032c4a082091d7481a1c66
Gbolahan Akande <geakande@gmail.com>
2026-03-26 11:26:00 +0100

    stacks: add StacksNetwork type

--- src/lib/stacks/stacks-common.ts
+++ src/lib/stacks/stacks-common.ts
@@ -7,3 +7,5 @@ export const TESTNET_CHAIN_ID = 2147483648;
 export const MAINNET_API_URL = 'https://api.hiro.so';
 /** Stacks testnet API base URL */
 export const TESTNET_API_URL = 'https://api.testnet.hiro.so';
+/** Network type */
+export type StacksNetwork = 'mainnet' | 'testnet' | 'devnet';
