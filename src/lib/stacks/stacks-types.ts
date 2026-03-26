6b38d5ca85dba46700410cdd7120be170c82d978
Gbolahan Akande <geakande@gmail.com>
2026-03-26 13:18:00 +0100

    stacks: add ContractCallOptions interface

--- src/lib/stacks/stacks-types.ts
+++ src/lib/stacks/stacks-types.ts
@@ -1,1 +1,9 @@
 // Shared TypeScript types for Stacks.js SDK integration
+/** Contract call options */
+export interface ContractCallOptions {
+  contractAddress: string;
+  contractName: string;
+  functionName: string;
+  functionArgs: unknown[];
+  network: string;
+}
