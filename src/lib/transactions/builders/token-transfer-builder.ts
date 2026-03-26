034106c4cd37816412e9334a8e732394dccfe158
Gbolahan Akande <geakande@gmail.com>
2026-03-26 17:18:00 +0100

    tx: add FT_BUILDER_CONST_7

--- src/lib/transactions/builders/token-transfer-builder.ts
+++ src/lib/transactions/builders/token-transfer-builder.ts
@@ -61,3 +61,4 @@ export const FT_BUILDER_CONST_3 = 3;
 export const FT_BUILDER_CONST_4 = 4;
 export const FT_BUILDER_CONST_5 = 5;
 export const FT_BUILDER_CONST_6 = 6;
+export const FT_BUILDER_CONST_7 = 7;
