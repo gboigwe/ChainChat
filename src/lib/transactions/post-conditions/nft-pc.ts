71971dbdf4877df1821d0c27951392bc18e2d82c
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:34:00 +0100

    tx: add makeStandardNonFungiblePostCondition function

--- src/lib/transactions/post-conditions/nft-pc.ts
+++ src/lib/transactions/post-conditions/nft-pc.ts
@@ -12,3 +12,20 @@ export interface StandardNFTPostCondition {
   assetInfo: { contractAddress: string; contractName: string; assetName: string };
   tokenId: unknown;
 }
+/** Make standard NFT post-condition */
+export function makeStandardNonFungiblePostCondition(
+  address: string,
+  conditionCode: NonFungibleConditionCode,
+  contractAddress: string,
+  contractName: string,
+  assetName: string,
+  tokenId: unknown,
+): StandardNFTPostCondition {
+  return {
+    type: 'nft',
+    principal: { type: 'standard', address },
+    conditionCode,
+    assetInfo: { contractAddress, contractName, assetName },
+    tokenId,
+  };
+}
