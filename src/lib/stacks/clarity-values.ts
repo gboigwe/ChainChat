d8606d421665fb70be248c87a443c2a3e37a4dab
Gbolahan Akande <geakande@gmail.com>
2026-03-26 16:40:00 +0100

    stacks: add arg14 convenience function

--- src/lib/stacks/clarity-values.ts
+++ src/lib/stacks/clarity-values.ts
@@ -174,3 +174,5 @@ export function arg11<T extends ClarityValue>(cv: T): T { return cv; }
 export function arg12<T extends ClarityValue>(cv: T): T { return cv; }
 /** CV builder convenience for arg position 13 */
 export function arg13<T extends ClarityValue>(cv: T): T { return cv; }
+/** CV builder convenience for arg position 14 */
+export function arg14<T extends ClarityValue>(cv: T): T { return cv; }
