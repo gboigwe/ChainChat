368d7957b5f9ff2c957f04159008206fcc2f9c8e
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:36:00 +0100

    utils: add additional validation constants

---
+++ src/utils/validation.ts
@@ -0,0 +1,6 @@
+
+export const VALIDATION_MIN_URI_LEN = 7;
+export const VALIDATION_MAX_URI_LEN = 256;
+export const VALIDATION_MIN_USERNAME_LEN = 2;
+export const VALIDATION_MAX_USERNAME_LEN = 32;
+export const VALIDATION_HEX_EVEN_LENGTH = true;
