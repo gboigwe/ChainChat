5cceb997ad904c044d8cd541572be6e5dd6894cc
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:10:00 +0100

    connect: add SignMessageOptions interface

--- src/lib/connect/message-signing.ts
+++ src/lib/connect/message-signing.ts
@@ -1,1 +1,8 @@
 // Stacks Connect message signing helpers
+/** Message signing options */
+export interface SignMessageOptions {
+  message: string;
+  onFinish?: (data: SignatureData) => void;
+  onCancel?: () => void;
+  network?: string;
+}
