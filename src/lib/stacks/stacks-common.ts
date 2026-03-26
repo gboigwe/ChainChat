649ccd7160c144bb86c3bb496326121c637a3e1e
Gbolahan Akande <geakande@gmail.com>
2026-03-26 11:24:00 +0100

    stacks: add TESTNET_API_URL constant

--- src/lib/stacks/stacks-common.ts
+++ src/lib/stacks/stacks-common.ts
@@ -5,3 +5,5 @@ export const MAINNET_CHAIN_ID = 1;
 export const TESTNET_CHAIN_ID = 2147483648;
 /** Stacks mainnet API base URL */
 export const MAINNET_API_URL = 'https://api.hiro.so';
+/** Stacks testnet API base URL */
+export const TESTNET_API_URL = 'https://api.testnet.hiro.so';
