02a990f1be045969f4953f58aa8b5d99c0963f72
Gbolahan Akande <geakande@gmail.com>
2026-03-26 17:30:00 +0100

    examples: add CREATE_CHAN_CONST_21

--- src/examples/create-channel.ts
+++ src/examples/create-channel.ts
@@ -60,3 +60,4 @@ export const CREATE_CHAN_CONST_17 = 17;
 export const CREATE_CHAN_CONST_18 = 18;
 export const CREATE_CHAN_CONST_19 = 19;
 export const CREATE_CHAN_CONST_20 = 20;
+export const CREATE_CHAN_CONST_21 = 21;
