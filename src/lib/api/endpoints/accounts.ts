4b6e43d37b001d8e72d6f3ac1b75b23243953897
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:26:00 +0100

    api: add AccountInfoResponse interface

--- src/lib/api/endpoints/accounts.ts
+++ src/lib/api/endpoints/accounts.ts
@@ -1,2 +1,11 @@
 // Account-related Hiro API endpoints
 import type { HiroApiClient } from '../client';
+/** Account info response */
+export interface AccountInfoResponse {
+  balance: string;
+  locked: string;
+  unlock_height: number;
+  nonce: number;
+  balance_proof: string;
+  nonce_proof: string;
+}
