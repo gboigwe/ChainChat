9ccf8450ec0da2ff7a1441555ff629352a8b38e8
Gbolahan Akande <geakande@gmail.com>
2026-03-26 18:02:00 +0100

    tx: add FT_BUILDER_CONST_29

--- src/lib/transactions/builders/token-transfer-builder.ts
+++ src/lib/transactions/builders/token-transfer-builder.ts
@@ -83,3 +83,4 @@ export const FT_BUILDER_CONST_25 = 25;
 export const FT_BUILDER_CONST_26 = 26;
 export const FT_BUILDER_CONST_27 = 27;
 export const FT_BUILDER_CONST_28 = 28;
+export const FT_BUILDER_CONST_29 = 29;
