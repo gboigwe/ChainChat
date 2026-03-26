190f498cdc364fe30e7a9510a401af277097f981
Gbolahan Akande <geakande@gmail.com>
2026-03-26 13:40:00 +0100

    utils: add async helper type constants

---
+++ src/utils/async-helpers.ts
@@ -0,0 +1,6 @@
+
+export const ASYNC_CONST_1 = 'timeout';
+export const ASYNC_CONST_2 = 'retry';
+export const ASYNC_CONST_3 = 'poll';
+export const ASYNC_CONST_4 = 'debounce';
+export const ASYNC_CONST_5 = 'throttle';
