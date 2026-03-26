1d1149480b76b3ab17eda1b1cec8a539896a055f
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:32:00 +0100

    tx: add StandardNFTPostCondition interface

--- src/lib/transactions/post-conditions/nft-pc.ts
+++ src/lib/transactions/post-conditions/nft-pc.ts
@@ -4,3 +4,11 @@ export enum NonFungibleConditionCode {
   DoesNotSend = 'not-sent',
   Sends = 'sent',
 }
+/** Standard NFT post-condition */
+export interface StandardNFTPostCondition {
+  type: 'nft';
+  principal: { type: 'standard'; address: string };
+  conditionCode: NonFungibleConditionCode;
+  assetInfo: { contractAddress: string; contractName: string; assetName: string };
+  tokenId: unknown;
+}
