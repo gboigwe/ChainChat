663e5547a8b11e2c1f4c124df3a0b7ea1ebd660b
Gbolahan Akande <geakande@gmail.com>
2026-03-26 13:42:00 +0100

    utils: add ASYNC_CONST_6 async constant

--- src/utils/async-helpers.ts
+++ src/utils/async-helpers.ts
@@ -4,3 +4,4 @@ export const ASYNC_CONST_2 = 'retry';
 export const ASYNC_CONST_3 = 'poll';
 export const ASYNC_CONST_4 = 'debounce';
 export const ASYNC_CONST_5 = 'throttle';
+export const ASYNC_CONST_6 = 6;
