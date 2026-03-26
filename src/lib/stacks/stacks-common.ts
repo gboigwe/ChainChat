e74220f901bea2df94b68b461ec0ec6a9d7fa114
Gbolahan Akande <geakande@gmail.com>
2026-03-26 11:20:00 +0100

    stacks: add TESTNET_CHAIN_ID constant

--- src/lib/stacks/stacks-common.ts
+++ src/lib/stacks/stacks-common.ts
@@ -1,3 +1,5 @@
 // Shared Stacks.js utilities and constants
 /** Stacks mainnet chain ID */
 export const MAINNET_CHAIN_ID = 1;
+/** Stacks testnet chain ID */
+export const TESTNET_CHAIN_ID = 2147483648;
