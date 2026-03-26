db13dc0ca6ec70152d22d477ae6d82f2e74f8b00
Gbolahan Akande <geakande@gmail.com>
2026-03-26 11:36:00 +0100

    clarity: add TESTNET_PRINCIPAL_REGEX constant

--- src/lib/clarity/principal-helpers.ts
+++ src/lib/clarity/principal-helpers.ts
@@ -2,3 +2,6 @@
 
 /** Regex for valid Stacks mainnet standard principal */
 const MAINNET_PRINCIPAL_REGEX = /^SP[0-9A-Z]{38,39}$/;
+
+/** Regex for valid Stacks testnet standard principal */
+const TESTNET_PRINCIPAL_REGEX = /^ST[0-9A-Z]{38,39}$/;
