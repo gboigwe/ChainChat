379ecd52ab19d978d8baaaf1ec04a8325ea95885
Gbolahan Akande <geakande@gmail.com>
2026-03-26 12:12:00 +0100

    utils: add CONV_CONST_7 conversion constant

--- src/utils/conversion.ts
+++ src/utils/conversion.ts
@@ -4,3 +4,4 @@ export const CONV_GENESIS_TESTNET = 1610000000;
 export const CONV_MIN_TRANSFER_STX = 0.000001;
 export const CONV_STX_DECIMALS = 6;
 export const CONV_CONST_6 = 6;
+export const CONV_CONST_7 = 7;
