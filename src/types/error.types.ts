6c6306f297dfa0ed6002220fe8ec4ac07a42083c
Gbolahan Akande <geakande@gmail.com>
2026-03-26 17:22:00 +0100

    types: add StacksError base class

--- src/types/error.types.ts
+++ src/types/error.types.ts
@@ -1,1 +1,7 @@
 // Typed error classes for Stacks operations
+export class StacksError extends Error {
+  constructor(message: string, public readonly code: string) {
+    super(message);
+    this.name = 'StacksError';
+  }
+}
