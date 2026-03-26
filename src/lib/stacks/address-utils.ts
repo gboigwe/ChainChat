9aa28dbcff7dd506515d7a78756868e65acc3c0f
Gbolahan Akande <geakande@gmail.com>
2026-03-26 11:56:00 +0100

    stacks: add AddressVersion enum

--- src/lib/stacks/address-utils.ts
+++ src/lib/stacks/address-utils.ts
@@ -1,3 +1,10 @@
 // Stacks address encoding and validation utilities
 /** C32 alphabet for Stacks addresses */
 const C32_ALPHABET = '0123456789ABCDEFGHJKMNPQRSTVWXYZ';
+/** Address version bytes */
+export enum AddressVersion {
+  MainnetSingleSig = 22,
+  MainnetMultiSig = 20,
+  TestnetSingleSig = 26,
+  TestnetMultiSig = 21,
+}
