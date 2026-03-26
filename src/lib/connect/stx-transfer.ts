14637d760546c52c343dec93124668f1d3f3eb9e
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:54:00 +0100

    connect: add validateTransferAmount function

--- src/lib/connect/stx-transfer.ts
+++ src/lib/connect/stx-transfer.ts
@@ -10,3 +10,10 @@ export interface STXTransferOptions {
   onFinish?: (data: { txId: string; txRaw: string }) => void;
   onCancel?: () => void;
 }
+/** Validate STX transfer amount */
+export function validateTransferAmount(amount: bigint): string[] {
+  const errors: string[] = [];
+  if (amount <= 0n) errors.push('Amount must be positive');
+  if (amount > 1_000_000_000_000_000n) errors.push('Amount exceeds maximum');
+  return errors;
+}
