9c46ba56f502a97d04527ede4b3c2451ae10f847
Gbolahan Akande <geakande@gmail.com>
2026-03-26 17:48:00 +0100

    types: add ERROR_TYPE_CONST_5

--- src/types/error.types.ts
+++ src/types/error.types.ts
@@ -46,3 +46,4 @@ export const ERROR_TYPE_CONST_1 = 1;
 export const ERROR_TYPE_CONST_2 = 2;
 export const ERROR_TYPE_CONST_3 = 3;
 export const ERROR_TYPE_CONST_4 = 4;
+export const ERROR_TYPE_CONST_5 = 5;
