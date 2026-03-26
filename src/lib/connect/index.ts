d17725d288394741eab3e256b5e25bdf344d205f
Gbolahan Akande <geakande@gmail.com>
2026-03-26 12:32:00 +0100

    connect: add CONNECT_CONST_4

--- src/lib/connect/index.ts
+++ src/lib/connect/index.ts
@@ -11,3 +11,5 @@ export const CONNECT_CONST_1 = 1;
 export const CONNECT_CONST_2 = 2;
 // connect module constant 3
 export const CONNECT_CONST_3 = 3;
+// connect module constant 4
+export const CONNECT_CONST_4 = 4;
