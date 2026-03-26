715fbf9e6b10de2453349ec86a8240cf18ba49ea
Gbolahan Akande <geakande@gmail.com>
2026-03-26 16:50:00 +0100

    connect: add TRANSFER_CONST_18

--- src/lib/connect/stx-transfer.ts
+++ src/lib/connect/stx-transfer.ts
@@ -75,3 +75,4 @@ export const TRANSFER_CONST_14 = 14;
 export const TRANSFER_CONST_15 = 15;
 export const TRANSFER_CONST_16 = 16;
 export const TRANSFER_CONST_17 = 17;
+export const TRANSFER_CONST_18 = 18;
