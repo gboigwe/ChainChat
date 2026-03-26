cc9a35ccbda7aca6013a14967e0decefbca83576
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:02:00 +0100

    test: scaffold post-conditions test file

---
+++ src/__tests__/post-conditions.test.ts
@@ -0,0 +1,15 @@
+// Post-condition tests for Stacks transactions
+export const PC_TEST_STX_AMOUNT = 1000000n;
+export const PC_TEST_FT_AMOUNT = 500n;
+export const PC_TEST_NFT_ID = 42n;
+export const PC_CONDITION_EQUAL = 'eq';
+export const PC_CONDITION_GT = 'gt';
+export const PC_CONDITION_LT = 'lt';
+export const PC_CONDITION_GTE = 'gte';
+export const PC_CONDITION_LTE = 'lte';
+export const PC_STANDARD_PRINCIPAL = 'SP2JXKMSH007NPYAQHKJPQMAQYAD90NQGTVJVQ02B';
+export const PC_CONTRACT_PRINCIPAL = 'SP2JXKMSH007NPYAQHKJPQMAQYAD90NQGTVJVQ02B.chainchat-v1';
+export const PC_TEST_FT_CONTRACT = 'SP2JXKMSH007NPYAQHKJPQMAQYAD90NQGTVJVQ02B.chat-token';
+export const PC_TEST_NFT_CONTRACT = 'SP2JXKMSH007NPYAQHKJPQMAQYAD90NQGTVJVQ02B.chat-nft';
+export const PC_TEST_ASSET_NAME = 'chat-token';
+export const PC_TEST_NFT_ASSET = 'chat-badge';
