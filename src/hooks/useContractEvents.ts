dbd37b5d5296757619c595803e92511b5722bc6c
Gbolahan Akande <geakande@gmail.com>
2026-03-26 17:44:00 +0100

    hooks: add CONTRACT_EVENTS_4 constant

--- src/hooks/useContractEvents.ts
+++ src/hooks/useContractEvents.ts
@@ -44,3 +44,4 @@ export function useContractEvents(
 export const CONTRACT_EVENTS_1 = 1;
 export const CONTRACT_EVENTS_2 = 2;
 export const CONTRACT_EVENTS_3 = 3;
+export const CONTRACT_EVENTS_4 = 4;
