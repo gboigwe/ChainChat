4b0ea85940f1f93dcbce3e03002fd9fb1b0e6e3e
Gbolahan Akande <geakande@gmail.com>
2026-03-26 18:56:00 +0100

    stacks: add ENCODING_CONSTANT_15

--- src/lib/stacks/cv-encoding.ts
+++ src/lib/stacks/cv-encoding.ts
@@ -118,3 +118,4 @@ export const ENCODING_CONSTANT_11 = 11;
 export const ENCODING_CONSTANT_12 = 12;
 export const ENCODING_CONSTANT_13 = 13;
 export const ENCODING_CONSTANT_14 = 14;
+export const ENCODING_CONSTANT_15 = 15;
