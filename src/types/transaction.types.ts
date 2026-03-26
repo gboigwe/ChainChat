64f70b802174e8326680216b880ba8c89d6e393f
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:28:00 +0100

    types: add TxStatus union

--- src/types/transaction.types.ts
+++ src/types/transaction.types.ts
@@ -1,2 +1,3 @@
 // Transaction type definitions
 export type TxType = 'coinbase' | 'token_transfer' | 'smart_contract' | 'contract_call' | 'poison_microblock';
+export type TxStatus = 'pending' | 'success' | 'abort_by_response' | 'abort_by_post_condition' | 'dropped_replace_by_fee' | 'dropped_too_expensive' | 'dropped_stale_garbage_collect';
