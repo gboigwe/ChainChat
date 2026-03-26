d88eb4f75cb23fdca6c5c69e20d28b51e4d547ee
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:46:00 +0100

    api: add TransactionResponse interface

--- src/lib/api/endpoints/transactions.ts
+++ src/lib/api/endpoints/transactions.ts
@@ -1,2 +1,13 @@
 // Transaction-related Hiro API endpoints
 import type { HiroApiClient } from '../client';
+/** Transaction response */
+export interface TransactionResponse {
+  tx_id: string;
+  nonce: number;
+  fee_rate: string;
+  sender_address: string;
+  tx_status: string;
+  block_hash?: string;
+  block_height?: number;
+  tx_index?: number;
+}
