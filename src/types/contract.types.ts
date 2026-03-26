7fa8bc7eb8b1356a3b75391b6772a215c484b2b0
Gbolahan Akande <geakande@gmail.com>
2026-03-26 12:30:00 +0100

    types: add CONTRACT_TYPE_CONST_14

--- src/types/contract.types.ts
+++ src/types/contract.types.ts
@@ -48,3 +48,4 @@ export const CONTRACT_TYPE_CONST_10 = 10;
 export const CONTRACT_TYPE_CONST_11 = 11;
 export const CONTRACT_TYPE_CONST_12 = 12;
 export const CONTRACT_TYPE_CONST_13 = 13;
+export const CONTRACT_TYPE_CONST_14 = 14;
