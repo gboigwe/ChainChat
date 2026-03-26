df1b2ac5d57f5ee348cbd39214ff13f9823eae7d
Gbolahan Akande <geakande@gmail.com>
2026-03-26 13:18:00 +0100

    types: add TypedClarityValue generic

--- src/types/clarity-value.types.ts
+++ src/types/clarity-value.types.ts
@@ -8,3 +8,7 @@ export type StringAsciiBrand = Branded<string, 'StringAscii'>;
 export type StringUtf8Brand = Branded<string, 'StringUtf8'>;
 export type BufferBrand = Branded<Uint8Array, 'Buffer'>;
 export type ClarityValueType = 'uint' | 'int' | 'bool' | 'string-ascii' | 'string-utf8' | 'buffer' | 'list' | 'tuple' | 'principal' | 'contract' | 'none' | 'some' | 'ok' | 'err';
+export interface TypedClarityValue<T extends ClarityValueType, V> {
+  type: T;
+  value: V;
+}
