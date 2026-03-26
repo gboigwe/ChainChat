1c4e5cc9bf91fe78ce86ed3662b5506b4a644aa9
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:02:00 +0100

    tx: add FungibleConditionCode enum

--- src/lib/transactions/post-conditions/stx-pc.ts
+++ src/lib/transactions/post-conditions/stx-pc.ts
@@ -1,1 +1,9 @@
 // STX post-condition builders
+/** Fungible condition codes for STX */
+export enum FungibleConditionCode {
+  Equal = 'sent-equal-to',
+  Greater = 'sent-greater-than',
+  GreaterEqual = 'sent-greater-than-or-equal-to',
+  Less = 'sent-less-than',
+  LessEqual = 'sent-less-than-or-equal-to',
+}
