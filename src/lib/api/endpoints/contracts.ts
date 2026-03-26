2054a68ee257e4c4c6d7859fe0b7f3b655bcbb22
Gbolahan Akande <geakande@gmail.com>
2026-03-26 17:50:00 +0100

    api: add CONTRACT_CONST_16

--- src/lib/api/endpoints/contracts.ts
+++ src/lib/api/endpoints/contracts.ts
@@ -83,3 +83,4 @@ export const CONTRACT_CONST_12 = 12;
 export const CONTRACT_CONST_13 = 13;
 export const CONTRACT_CONST_14 = 14;
 export const CONTRACT_CONST_15 = 15;
+export const CONTRACT_CONST_16 = 16;
