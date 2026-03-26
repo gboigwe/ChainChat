aae90c15aee74ef8350978d6816f568bdfc0d36f
Gbolahan Akande <geakande@gmail.com>
2026-03-26 12:04:00 +0100

    tx: export token-transfer-builder from index

--- src/lib/transactions/index.ts
+++ src/lib/transactions/index.ts
@@ -1,3 +1,4 @@
 // Stacks transactions SDK — public API
 export * from './builders/contract-call-builder';
 export * from './builders/stx-transfer-builder';
+export * from './builders/token-transfer-builder';
