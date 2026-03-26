a133f5e0d04df929000984d4d469e339099b5c0f
Gbolahan Akande <geakande@gmail.com>
2026-03-26 18:50:00 +0100

    storage: add STORAGE_CONSTANT_5

--- src/lib/clarity/storage/constants.ts
+++ src/lib/clarity/storage/constants.ts
@@ -30,3 +30,4 @@ export const STORAGE_CONSTANT_1 = 1;
 export const STORAGE_CONSTANT_2 = 2;
 export const STORAGE_CONSTANT_3 = 3;
 export const STORAGE_CONSTANT_4 = 4;
+export const STORAGE_CONSTANT_5 = 5;
