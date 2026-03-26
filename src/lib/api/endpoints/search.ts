7819ac4536a0c62c1fbd8f1f1d8af91baf963a44
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:36:00 +0100

    api: add SearchResult interface

--- src/lib/api/endpoints/search.ts
+++ src/lib/api/endpoints/search.ts
@@ -2,3 +2,8 @@
 import type { HiroApiClient } from '../client';
 /** Search result type union */
 export type SearchResultType = 'tx' | 'block' | 'contract_id' | 'standard_address' | 'unknown_hash';
+/** Generic search result */
+export interface SearchResult {
+  found: boolean;
+  result?: { entity_type: SearchResultType; entity_id: string };
+}
