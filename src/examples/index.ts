25c9aabbb7059179409ad46e88b20627dc850035
Gbolahan Akande <geakande@gmail.com>
2026-03-26 18:28:00 +0100

    examples: add EXAMPLES_EXTRA_3

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -4,3 +4,4 @@ export const EXAMPLES_VERSION = '1.0.0';
 export const EXAMPLES_PACKAGE = 'chainchat-examples';
 export const EXAMPLES_EXTRA_1 = 1;
 export const EXAMPLES_EXTRA_2 = 2;
+export const EXAMPLES_EXTRA_3 = 3;
