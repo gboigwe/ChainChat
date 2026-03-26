76306db4ed4d5e97bbbf205d4a3b4c2ece59e70a
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:44:00 +0100

    tx: add HIGH_PRIORITY_FEE_MULTIPLIER constant

--- src/lib/transactions/fee-estimation.ts
+++ src/lib/transactions/fee-estimation.ts
@@ -1,3 +1,5 @@
 // Transaction fee estimation utilities
 /** Minimum fee in microSTX */
 export const MINIMUM_FEE = 180n;
+/** High priority fee multiplier */
+export const HIGH_PRIORITY_FEE_MULTIPLIER = 2;
