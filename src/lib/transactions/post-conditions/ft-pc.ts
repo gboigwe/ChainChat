6c5b67973e2fed9145b8ec092554602c0a00c0f0
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:20:00 +0100

    tx: add StandardFTPostCondition interface

--- src/lib/transactions/post-conditions/ft-pc.ts
+++ src/lib/transactions/post-conditions/ft-pc.ts
@@ -1,2 +1,10 @@
 // Fungible token post-condition builders
 import { FungibleConditionCode } from './stx-pc';
+/** FT post-condition structure */
+export interface StandardFTPostCondition {
+  type: 'ft';
+  principal: { type: 'standard'; address: string };
+  conditionCode: FungibleConditionCode;
+  amount: bigint;
+  assetInfo: { contractAddress: string; contractName: string; assetName: string };
+}
