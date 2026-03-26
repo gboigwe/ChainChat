eca9b50d1839fe211d344a05db4ee08ce7cb2c05
Gbolahan Akande <geakande@gmail.com>
2026-03-26 16:30:00 +0100

    examples: add SEND_MSG_CONST_27

--- src/examples/send-chat-message.ts
+++ src/examples/send-chat-message.ts
@@ -60,3 +60,4 @@ export const SEND_MSG_CONST_23 = 23;
 export const SEND_MSG_CONST_24 = 24;
 export const SEND_MSG_CONST_25 = 25;
 export const SEND_MSG_CONST_26 = 26;
+export const SEND_MSG_CONST_27 = 27;
