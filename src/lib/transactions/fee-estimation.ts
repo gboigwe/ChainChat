415c6b249523d6e406f228eeecfb3ba01c99b8c1
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:46:00 +0100

    tx: add LOW_PRIORITY_FEE_MULTIPLIER constant

--- src/lib/transactions/fee-estimation.ts
+++ src/lib/transactions/fee-estimation.ts
@@ -3,3 +3,5 @@
 export const MINIMUM_FEE = 180n;
 /** High priority fee multiplier */
 export const HIGH_PRIORITY_FEE_MULTIPLIER = 2;
+/** Low priority fee (50% of standard) */
+export const LOW_PRIORITY_FEE_MULTIPLIER = 0.5;
