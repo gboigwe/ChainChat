c5ffd7355106a1013f004f6f9690be8922add900
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:20:00 +0100

    api: add BlockResponse interface

--- src/lib/api/endpoints/blocks.ts
+++ src/lib/api/endpoints/blocks.ts
@@ -1,2 +1,15 @@
 // Block-related Hiro API endpoints
 import type { HiroApiClient } from '../client';
+/** Block response */
+export interface BlockResponse {
+  canonical: boolean;
+  height: number;
+  hash: string;
+  index_block_hash: string;
+  parent_block_hash: string;
+  burn_block_time: number;
+  burn_block_time_iso: string;
+  burn_block_hash: string;
+  burn_block_height: number;
+  txs: string[];
+}
