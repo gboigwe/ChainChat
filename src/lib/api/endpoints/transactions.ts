59907eb73c5ef2ab111c34c670bf3cab18480a12
Gbolahan Akande <geakande@gmail.com>
2026-03-26 16:08:00 +0100

    api: add TX_ENDPOINT_CONST_5

--- src/lib/api/endpoints/transactions.ts
+++ src/lib/api/endpoints/transactions.ts
@@ -81,3 +81,4 @@ export const TX_ENDPOINT_CONST_1 = 1;
 export const TX_ENDPOINT_CONST_2 = 2;
 export const TX_ENDPOINT_CONST_3 = 3;
 export const TX_ENDPOINT_CONST_4 = 4;
+export const TX_ENDPOINT_CONST_5 = 5;
