13ca3cc5a271028c1062dbde23cf81039ddf712f
Gbolahan Akande <geakande@gmail.com>
2026-03-26 16:42:00 +0100

    api: add TX_ENDPOINT_CONST_22

--- src/lib/api/endpoints/transactions.ts
+++ src/lib/api/endpoints/transactions.ts
@@ -98,3 +98,4 @@ export const TX_ENDPOINT_CONST_18 = 18;
 export const TX_ENDPOINT_CONST_19 = 19;
 export const TX_ENDPOINT_CONST_20 = 20;
 export const TX_ENDPOINT_CONST_21 = 21;
+export const TX_ENDPOINT_CONST_22 = 22;
