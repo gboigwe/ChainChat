3e002fbfaf66fc4947bdef7620423e7be7bd478d
Gbolahan Akande <geakande@gmail.com>
2026-03-26 17:18:00 +0100

    types: add API_RESPONSE_CONST_35

--- src/types/api-response.types.ts
+++ src/types/api-response.types.ts
@@ -74,3 +74,4 @@ export const API_RESPONSE_CONST_31 = 31;
 export const API_RESPONSE_CONST_32 = 32;
 export const API_RESPONSE_CONST_33 = 33;
 export const API_RESPONSE_CONST_34 = 34;
+export const API_RESPONSE_CONST_35 = 35;
