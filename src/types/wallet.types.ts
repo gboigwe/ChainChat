895d6088fff21f1ae847d559ccff0dc5f101fb6e
Gbolahan Akande <geakande@gmail.com>
2026-03-26 15:00:00 +0100

    types: add WALLET_TYPE_CONST_6

--- src/types/wallet.types.ts
+++ src/types/wallet.types.ts
@@ -41,3 +41,4 @@ export const WALLET_TYPE_CONST_2 = 2;
 export const WALLET_TYPE_CONST_3 = 3;
 export const WALLET_TYPE_CONST_4 = 4;
 export const WALLET_TYPE_CONST_5 = 5;
+export const WALLET_TYPE_CONST_6 = 6;
