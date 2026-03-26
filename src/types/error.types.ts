625074e1e76f9f38b32cd9df5761faf27ac264c0
Gbolahan Akande <geakande@gmail.com>
2026-03-26 18:04:00 +0100

    types: add ERROR_TYPE_CONST_13

--- src/types/error.types.ts
+++ src/types/error.types.ts
@@ -54,3 +54,4 @@ export const ERROR_TYPE_CONST_9 = 9;
 export const ERROR_TYPE_CONST_10 = 10;
 export const ERROR_TYPE_CONST_11 = 11;
 export const ERROR_TYPE_CONST_12 = 12;
+export const ERROR_TYPE_CONST_13 = 13;
