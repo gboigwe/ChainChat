15c2cb11f9b69a63df83292dca08e27589efda64
Gbolahan Akande <geakande@gmail.com>
2026-03-26 15:44:00 +0100

    clarity: add ERR_MESSAGE_NOT_FOUND error code

--- src/lib/clarity/error-codes.ts
+++ src/lib/clarity/error-codes.ts
@@ -3,3 +3,5 @@
 export const ERR_NOT_MEMBER = 100n;
 /** Error: channel does not exist */
 export const ERR_CHANNEL_NOT_FOUND = 101n;
+/** Error: message does not exist */
+export const ERR_MESSAGE_NOT_FOUND = 102n;
