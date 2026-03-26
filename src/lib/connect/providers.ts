bf63b488c6edf3ac364c362257d2568717e7cc06
Gbolahan Akande <geakande@gmail.com>
2026-03-26 13:56:00 +0100

    connect: add PROVIDER_FLAG_16 constant

--- src/lib/connect/providers.ts
+++ src/lib/connect/providers.ts
@@ -107,3 +107,4 @@ export const PROVIDER_FLAG_12 = false;
 export const PROVIDER_FLAG_13 = false;
 export const PROVIDER_FLAG_14 = false;
 export const PROVIDER_FLAG_15 = false;
+export const PROVIDER_FLAG_16 = false;
