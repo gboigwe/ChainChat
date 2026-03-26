cc048c5ab55554bc995aea18cecc843b1381a581
Gbolahan Akande <geakande@gmail.com>
2026-03-26 13:32:00 +0100

    hooks: add CONTRACT_WRITE_18 constant

--- src/hooks/useContractWrite.ts
+++ src/hooks/useContractWrite.ts
@@ -66,3 +66,4 @@ export const CONTRACT_WRITE_14 = 14;
 export const CONTRACT_WRITE_15 = 15;
 export const CONTRACT_WRITE_16 = 16;
 export const CONTRACT_WRITE_17 = 17;
+export const CONTRACT_WRITE_18 = 18;
