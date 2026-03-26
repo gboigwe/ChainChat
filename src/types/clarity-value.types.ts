4763ce7b9ed9bbabbf358f4b3c0b7c7223001652
Gbolahan Akande <geakande@gmail.com>
2026-03-26 13:14:00 +0100

    types: add CV branded type aliases

--- src/types/clarity-value.types.ts
+++ src/types/clarity-value.types.ts
@@ -1,3 +1,9 @@
 // ClarityValue branded types
 declare const brand: unique symbol;
 export type Branded<T, B> = T & { readonly [brand]: B };
+export type UIntBrand = Branded<bigint, 'UInt'>;
+export type IntBrand = Branded<bigint, 'Int'>;
+export type BoolBrand = Branded<boolean, 'Bool'>;
+export type StringAsciiBrand = Branded<string, 'StringAscii'>;
+export type StringUtf8Brand = Branded<string, 'StringUtf8'>;
+export type BufferBrand = Branded<Uint8Array, 'Buffer'>;
