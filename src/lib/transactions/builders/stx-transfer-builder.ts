ea9ec1773ce4a8081e27cc72e2e788d2b2f654c3
Gbolahan Akande <geakande@gmail.com>
2026-03-26 16:50:00 +0100

    tx: add STX_BUILDER_CONST_8

--- src/lib/transactions/builders/stx-transfer-builder.ts
+++ src/lib/transactions/builders/stx-transfer-builder.ts
@@ -72,3 +72,4 @@ export const STX_BUILDER_CONST_4 = 4;
 export const STX_BUILDER_CONST_5 = 5;
 export const STX_BUILDER_CONST_6 = 6;
 export const STX_BUILDER_CONST_7 = 7;
+export const STX_BUILDER_CONST_8 = 8;
