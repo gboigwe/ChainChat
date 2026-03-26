b85f223347242e56c35ae6fc35c52d6375ed15e1
Gbolahan Akande <geakande@gmail.com>
2026-03-26 11:52:00 +0100

    types: add ContractInterface interface

--- src/types/contract.types.ts
+++ src/types/contract.types.ts
@@ -3,3 +3,12 @@ export interface ContractIdentifier {
   contractAddress: string;
   contractName: string;
 }
+export interface ContractInterface {
+  functions: FunctionABI[];
+  variables: VariableABI[];
+  maps: MapABI[];
+  fungible_tokens: TokenABI[];
+  non_fungible_tokens: TokenABI[];
+  epoch: string;
+  clarity_version: string;
+}
