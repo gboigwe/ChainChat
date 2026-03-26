fd9f2ddcbf858c490e1bdf9839304fa0479f765f
Gbolahan Akande <geakande@gmail.com>
2026-03-26 15:48:00 +0100

    api: add ACCOUNT_CONST_35

--- src/lib/api/endpoints/accounts.ts
+++ src/lib/api/endpoints/accounts.ts
@@ -104,3 +104,4 @@ export const ACCOUNT_CONST_31 = 31;
 export const ACCOUNT_CONST_32 = 32;
 export const ACCOUNT_CONST_33 = 33;
 export const ACCOUNT_CONST_34 = 34;
+export const ACCOUNT_CONST_35 = 35;
