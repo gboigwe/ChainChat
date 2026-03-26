21b2857838e973848f397ed731ec411a9e53f0a6
Gbolahan Akande <geakande@gmail.com>
2026-03-26 14:24:00 +0100

    hooks: add ACCOUNT_BALANCE_16 constant

--- src/hooks/useAccountBalance.ts
+++ src/hooks/useAccountBalance.ts
@@ -55,3 +55,4 @@ export const ACCOUNT_BALANCE_12 = 12;
 export const ACCOUNT_BALANCE_13 = 13;
 export const ACCOUNT_BALANCE_14 = 14;
 export const ACCOUNT_BALANCE_15 = 15;
+export const ACCOUNT_BALANCE_16 = 16;
