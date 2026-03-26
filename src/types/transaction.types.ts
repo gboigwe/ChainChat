7140a50fd8f3ad7685ce272a1fdbacb269c76d7a
Gbolahan Akande <geakande@gmail.com>
2026-03-26 11:26:00 +0100

    types: add TX_TYPE_CONST_23

--- src/types/transaction.types.ts
+++ src/types/transaction.types.ts
@@ -65,3 +65,4 @@ export const TX_TYPE_CONST_19 = 19;
 export const TX_TYPE_CONST_20 = 20;
 export const TX_TYPE_CONST_21 = 21;
 export const TX_TYPE_CONST_22 = 22;
+export const TX_TYPE_CONST_23 = 23;
