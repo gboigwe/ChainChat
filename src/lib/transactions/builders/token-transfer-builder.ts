dd439358d913e256b6b6558c9d2354bb05cfa722
Gbolahan Akande <geakande@gmail.com>
2026-03-26 11:38:00 +0100

    tx: add FTTransferTxOptions interface

--- src/lib/transactions/builders/token-transfer-builder.ts
+++ src/lib/transactions/builders/token-transfer-builder.ts
@@ -1,1 +1,14 @@
 // FT (fungible token) transfer transaction builder
+/** FT transfer transaction options */
+export interface FTTransferTxOptions {
+  contractAddress: string;
+  contractName: string;
+  tokenName: string;
+  recipient: string;
+  amount: bigint;
+  memo?: string;
+  fee: bigint;
+  nonce: bigint;
+  network: string;
+  anchorMode: 'on-chain-only' | 'off-chain-only' | 'any';
+}
