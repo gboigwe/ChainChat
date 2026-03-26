dc4504d159b958056d8bd82e12cef962ae2daad3
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:52:00 +0100

    connect: add STXTransferOptions interface

--- src/lib/connect/stx-transfer.ts
+++ src/lib/connect/stx-transfer.ts
@@ -1,1 +1,12 @@
 // Stacks Connect STX transfer helpers
+/** STX transfer options */
+export interface STXTransferOptions {
+  recipient: string;
+  amount: bigint;
+  memo?: string;
+  network: string;
+  fee?: bigint;
+  nonce?: bigint;
+  onFinish?: (data: { txId: string; txRaw: string }) => void;
+  onCancel?: () => void;
+}
