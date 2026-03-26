dd5ad80fbd261eddf70d88294ccc10f315201d9f
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:38:00 +0100

    utils: add VALID_CONST_6 validation constant

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -4,3 +4,4 @@ export const VALIDATION_MAX_URI_LEN = 256;
 export const VALIDATION_MIN_USERNAME_LEN = 2;
 export const VALIDATION_MAX_USERNAME_LEN = 32;
 export const VALIDATION_HEX_EVEN_LENGTH = true;
+export const VALID_CONST_6 = 6;
